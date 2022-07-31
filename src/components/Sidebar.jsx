5a9f5d2cbea1e748b8b37ad803040512989217c1
Yan Santos <yansantos.dev@gmail.com>
2022-07-31 11:23:54 -0300

    estilizando componente sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -8,12 +10,20 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1618695497554-6fdbb916806b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=50"
       />
 
-      <div className={styles.profiles}>
-        <strong>Diego Fernandes </strong>
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/40508828?v=4"
+        />
+
+        <strong>Yan Santos</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
