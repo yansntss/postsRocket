eefcc6ec724e2348a014a3f3ad979040c3cb9791
Yan Santos <yansantos.dev@gmail.com>
2022-08-01 09:42:23 -0300

    Formulário de comentário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -23,7 +23,7 @@ export function Post() {
       <div className={styles.content}>
         <p>Fala galeraa 👋 </p>
         <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          Acabei de subir mais um projeto no meu portfolio. É um projeto que fiz
           no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
         <p>
@@ -35,6 +35,19 @@ export function Post() {
           <a href="#">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+        <footer>
+          <button type="submit">Publicar</button>
+
+        </footer>
+      </form>
+
     </article>
   );
 }
